1cf83fcf34671ab1ee15a90bd170ee4c65334872
Shane Reynolds <shane.k.reynolds@gmail.com>
2020-04-13 10:40:55 +0930

    Update: created a plotting script and some images for use in blog post

---
+++ 01_single_area_models/01_P_control/02_simulation/plotting_script.m
@@ -0,0 +1,18 @@
+% Script for plotting the output from Simulink analysis
+
+% Plot of step change in load demand
+subplot(2,1,1)
+plot(tout,yout(:,2));
+xlabel('time (seconds)');
+ylabel('\Delta P_C(pu)');
+axis([0 11 -0.05 0.25]);
+
+% Plot of frequency reponse of system
+subplot(2,1,2)
+plot(tout,yout(:,1));
+xlabel('time (seconds)');
+ylabel('\Delta f (Hertz)');
+axis([0 11 -0.02 0]);
+
+% Save a copy of the simulimk model
+print(['-s',ModelName],'-dsvg',[ModelName,'.svg'])
